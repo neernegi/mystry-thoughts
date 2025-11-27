9f4b316e7bd12c6177c744b8f98dbbcca5265bfb
Neeraj <neerajnegi3443@gmail.com>
2025-11-27 11:50:13 +0530

    fix confession error

--- src/model/confession.ts
+++ src/model/confession.ts
@@ -1,15 +1,13 @@
 import mongoose, { Document, Schema, Types } from "mongoose";
 
-// Main IConfession interface
 export interface IConfession extends Document {
-  _id: string; // Explicitly adding string for frontend compatibility
+  _id: string; 
   user: Types.ObjectId;
   confession: string;
   createdAt: Date;
   updatedAt: Date;
 }
 
-// Schema for confession (No replies array)
 const ConfessionSchema: Schema<IConfession> = new Schema(
   {
     user: { type: Schema.Types.ObjectId, ref: "User", required: true },
